b626167f5d1c705e6ecf1c94c5d0dc9f93b6672d
gabriel.moraes <gabriel.loff@acad.pucrs.br>
2019-06-21 13:45:57 -0300

    Fluxo de login Inicio

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import Model.AbstractCliente;
+
+public class LoginController {
+
+    public LoginController() {
+    }
+
+    public AbstractCliente logar(String usuario, String senha){
+        //chama DAO para retornar cliente
+
+        //valida senha do usuario
+
+        //caso correta retorna objeto cliente
+
+        return null;
+    }
+
+}
--- src/Router/Router.java
+++ src/Router/Router.java
@@ -1,9 +1,12 @@
 package Router;
 
+import Controller.LoginController;
+import Model.AbstractCliente;
 import Model.Header;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -27,7 +30,10 @@ public class Router {
             System.out.println("Cliente conectado: " + client.getInetAddress().getHostAddress());
 
             ObjectInputStream entrada = new ObjectInputStream(client.getInputStream());
+            ObjectOutputStream saida = new ObjectOutputStream(client.getOutputStream());
             Header header = (Header) entrada.readObject();
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -35,6 +41,28 @@ public class Router {
             e.printStackTrace();
         }
 
+    }
+
+    public boolean rout(String servico,ObjectInputStream entrada, ObjectOutputStream saida) throws IOException, ClassNotFoundException {
+        switch (servico){
+            case "Login":
+                String usuario = (String) entrada.readObject();
+                String senha = (String) entrada.readObject();
+                //Chamar controller de login
+                LoginController loginController = new LoginController();
+                AbstractCliente cliente = loginController.logar(usuario,senha);
+                saida.flush();
+                saida.writeObject(cliente);
+                break;
+            default:
+                System.out.println("Serviço Invalido");
+                return false;
+        }
+
+
+
+
+        return true;
     }
 
 }
