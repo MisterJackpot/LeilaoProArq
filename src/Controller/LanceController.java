f16d3bfee0e702a1d7383eb90540995ddd29553b
gabriel.moraes <gabriel.loff@acad.pucrs.br>
2019-06-21 11:21:55 -0300

    Planejamento dar Lance

--- src/Controller/LanceController.java
+++ src/Controller/LanceController.java
@@ -1,4 +1,23 @@
 package Controller;
 
+import Model.Lance;
+
 public class LanceController {
+
+    public LanceController() {
+    }
+
+    public boolean darLance(int leilaoID, double valor, int cliente){
+        // getCliente
+
+        //getLeilao
+
+        // Validar valor do lance com valor do leilao (Talvez usar proxy)
+
+        //new Lance()
+
+        //darLance(Lance lance)
+
+        return true;
+    }
 }
---
+++ src/Model/Comprador.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Comprador extends AbstractCliente {
+
+
+    public Comprador(int id, String nome) {
+        super(id, nome);
+    }
+
+}
