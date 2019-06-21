9cab540a7fd9bb53bd0624a7b3e221e6c1585ca4
gabriel.moraes <gabriel.loff@acad.pucrs.br>
2019-06-21 15:07:04 -0300

    Tela Login

--- src/Front/View/LoginView.java
+++ src/Front/View/LoginView.java
@@ -7,7 +7,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -19,6 +22,14 @@ public class LoginView extends Application {
     @FXML
     private Label title;
 
+    @FXML
+    private TextField user;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button entrar;
 
     public static void main(String[] args) {
         launch(args);
@@ -31,11 +42,17 @@ public class LoginView extends Application {
             primaryStage.setTitle("Sistema Leilao");
             Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
             Scene appScene = new Scene(root);
+            primaryStage.setResizable(true);
             primaryStage.setScene(appScene);
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    @FXML
+    public void entrarAction(){
+        System.out.println("entrei");
     }
 }
